820f4ae976f16c4a896293bd9036b67c39becbe8
khodani-M <khodanimufamadi@gmail.com>
2022-10-31 11:06:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.button('Click')
 st.radio('Pick your gender',['Male','Female'])
 st.selectbox('Pick your gender',['Male','Female'])
 st.write("Line Chart in Streamlit")
-# 10 * 2 dimensional data
+
 chart_data = pd.DataFrame(
     np.random.randn(10, 2),
     columns=[f"Col{i+1}" for i in range(2)]

21fc5b520765d8b3b0f08c754f83f56decb47c2f
khodani-M <khodanimufamadi@gmail.com>
2022-10-31 11:06:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ st.checkbox('yes')
 st.button('Click')
 st.radio('Pick your gender',['Male','Female'])
 st.selectbox('Pick your gender',['Male','Female'])
-st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-st.slider('Pick a number', 0,50)
+st.write("Line Chart in Streamlit")
+# 10 * 2 dimensional data
+chart_data = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=[f"Col{i+1}" for i in range(2)]
+)
+
+st.line_chart(chart_data)
